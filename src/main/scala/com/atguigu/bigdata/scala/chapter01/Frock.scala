64bd1f90f69cff1c8513c3323efeb0a472656178
Fzfor <346224813@qq.com>
2020-02-02 10:55:56 +0800

    lianxi3

--- src/main/scala/com/atguigu/bigdata/scala/chapter01/Frock.scala
+++ src/main/scala/com/atguigu/bigdata/scala/chapter01/Frock.scala
@@ -8,7 +8,10 @@ package com.atguigu.bigdata.scala.chapter01
 
 object Frock {
   def main(args: Array[String]): Unit = {
-    println()
+    println("****")
+    println("****")
+    println("****")
+    println("****")
   }
   private var currentNum : Int = 10000
 

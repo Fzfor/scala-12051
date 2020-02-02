c86d64ca753f744fefeca30d1bf1ba265400298a
Fzfor <346224813@qq.com>
2020-02-02 10:11:07 +0800

    lianxi

--- src/main/scala/com/atguigu/bigdata/scala/chapter01/Frock.scala
+++ src/main/scala/com/atguigu/bigdata/scala/chapter01/Frock.scala
@@ -7,11 +7,15 @@ package com.atguigu.bigdata.scala.chapter01
 
 
 object Frock {
+  def main(args: Array[String]): Unit = {
+    println()
+  }
   private var currentNum : Int = 10000
 
   def getNextNum():Int ={
     currentNum = currentNum + 100
     currentNum
+
   }
 
 }
